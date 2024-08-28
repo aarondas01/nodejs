6ef9772a829203e0026e479dbd1207dd6cff316d
Aaron Das <aarononnet1@gmail.com>
2024-08-28 08:00:36 +0530

    Initial commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require("request");
+
+const geocode = (address, callback) => {
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    address +
+    ".json?access_token=pk.eyJ1IjoiYWFyb25kYXMwMSIsImEiOiJjbTBhdzdiZ2kwMXdrMmtzanp3bXZlZHZzIn0.9NJqpoJ7tjzR-hdrE2xJLw&limit=1";
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to location services!", undefined);
+    } else if (body.features.length === 0) {
+      callback("Unable to find location. Try another search.", undefined);
+    } else {
+      callback(undefined, {
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name,
+      });
+    }
+  });
+};
+
+module.exports = geocode;
